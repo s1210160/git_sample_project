b7647e76aa37742869f059ad8d63fc38f5eb3cd5
Haruna Nakazawa <s1210160@stdrsv3126.u-aizu.ac.jp>
2014-04-24 17:21:59 +0900

    add C Programing

---
+++ calc.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 50;
+int dtoi(char);
+main()
+{
+  FILE *fp;
+  int i, j, a;
+  char non[MAX], grade[5][MAX];
+
+  fp = fopen("grade.csv", "r");
+  if(fp == NULL)
+    {
+      printf("file open error.\n");
+      exit(0);
+    }
+
+  for(i=0; i<MAX; i++)
+    {
+      a = fscanf(fp, "%c", &non[i]);
+      if(a == '\n') break;
+    }
+
+  while(1)
+    {
+      b = fgetc(fp);
+      if(b >= '0' && b <= '9')
+	{
+	  
+    }
+    }
+  for(i=0; i<5; i++)
+    {
+      for(j=0; j<MAX; j++)
+	{
+	  printf("%d", grade[i][j]);
+	}
+    }
+  fclose(fp);
+}
+
+
+int dtoi(char c)
+{
+  switch(c)
+    {
+    case '0':return 0;
+    case '1':return 1;
+    case '2':return 2;
+    case '3':return 3;
+    case '4':return 4;
+    case '5':return 5;
+    case '6':return 6;
+    case '7':return 7;
+    case '8':return 8;
+    case '9':return 9;
+    }
+}
